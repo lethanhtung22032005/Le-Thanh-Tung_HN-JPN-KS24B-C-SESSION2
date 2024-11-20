3aa9dc31678d37a319a4f2edf620d41019a19268
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2024-11-20 23:59:47 +0700

    Add files via upload

---
+++ Session2 Ex2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	//Khai bao va khoi tao so nguye
+	int num1 = 10;//So nguyen thu nhat
+	int num2 = 20;//So nguyen thu hai
+	
+	// Tong hai so nguyen la
+	int sum = num1 + num2;
+	
+	//ket qua 2 so la 
+	printf("So nguyen thu nhat la: %d\n", num1);
+	printf("So nguyen thu hai la: %d\n", num2);
+	printf("Tong Hai so la: %d\n", sum);
+	
+	return 0;
+}
+     
